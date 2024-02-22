117b42f160b1804dc62977e864069ffd52aa6fa8
LucasAmadio1 <lucasrochaamadio02@gmail.com>
2024-02-22 00:00:28 -0300

    limpar o conteudo apos fechar o modal

--- src/componets/new-note-card.tsx
+++ src/componets/new-note-card.tsx
@@ -83,6 +83,10 @@ export function NewNoteCard({ onNoteCreated }: newNoteCardProps ) {
       }
     }
 
+    function handleClearContentAfterClosingModal() {
+      return setContent('')
+    }
+
     return (
     <Dialog.Root>
       <Dialog.Trigger className="rounded-md flex flex-col bg-slate-700 text-left p-5 gap-3 outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
@@ -98,7 +102,7 @@ export function NewNoteCard({ onNoteCreated }: newNoteCardProps ) {
       <Dialog.Portal>
           <Dialog.Overlay className="inset-0 fixed bg-black/50" />
           <Dialog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] h-[60vh] w-full bg-slate-700 rounded-md flex flex-col outline-none">
-            <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
+            <Dialog.Close onClick={handleClearContentAfterClosingModal} className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
               <X className="size-5" />
             </Dialog.Close>
 
